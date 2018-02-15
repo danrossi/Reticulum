48b5e2ef089439a4d4bf6b912bbcd6b7720e9942
Daniel Rossi <electroteque@gmail.com>
2018-02-15 21:31:48 +1100

    fixes

--- src/ReticleUtil.js
+++ src/ReticleUtil.js
@@ -107,7 +107,7 @@ export default class ReticleUtil {
           },
           transparent: transparent,
           vertexShader: ReticleUtil.vertexMorphShader,
-          fragmentShader: ReticleUtil.fragmentShader,
+          fragmentShader: ReticleUtil.fragmentShader
          
       });
     }
